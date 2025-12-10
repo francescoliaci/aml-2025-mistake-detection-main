2c2d1a360848a14ece84293e4582a1df8b2aa30c
s349552 <s296707@studenti.polito.it>
2025-12-10 15:20:34 +0100

    removed wandb

--- core/utils.py
+++ core/utils.py
@@ -1,27 +1,22 @@
 import sys
-
-import wandb
 from loguru import logger
-
 from constants import Constants as const
 
-
 def init_logger_and_wandb(config):
-    wandb.init(
-        project=config.model_name,
-        config=config,
-    )
-    config = {
+    # SOLO logger, niente wandb
+    logger_config = {
         "handlers": [
             {
                 "sink": sys.stdout,
-                "format": "<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | {module}.{function} | <level>{message}</level> ",
+                "format": "<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | {module}.{function} | <level>{message}</level>",
             },
             {
                 "sink": "logging/" + "logger_{time}.log",
-                "format": "<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | {module}.{function} | <level>{message}</level> ",
+                "format": "<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | {module}.{function} | <level>{message}</level>",
             },
         ],
         "extra": {"user": "usr"},
     }
-    logger.configure(**config)
+
+    logger.configure(**logger_config)
+    logger.info(f"Logger initialized. Training with config: {config.__dict__}")
